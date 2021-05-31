331810452a514fef895caadd6c4a4b10efa91b59
marVkus <markus_home@mail.ru>
2021-05-31 13:00:06 +0300

    implemented finding user location

--- RESTestTasks/RESTestTasks/UI/Main/MainViewController.swift
+++ RESTestTasks/RESTestTasks/UI/Main/MainViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreLocation
 
 class MainViewController: UIViewController {
     
@@ -24,6 +25,9 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    private let locationManager = CLLocationManager()
+    
+    private var userLocationCoordinate: CLLocationCoordinate2D?
     
     
     // MARK: - Lifecycle
@@ -31,12 +35,23 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.startUpdatingLocation()
+            
+        }
+        
         scrollView.delegate = self
         tableView.dataSource = self
         tableView.delegate = self
         
         let nib = UINib(nibName: String(describing: MainTableViewCell.self), bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: MainTableViewCell.cellID)
+        
+        
         
         
     }
@@ -83,19 +98,43 @@ extension MainViewController: UITableViewDelegate {
 extension MainViewController: UIScrollViewDelegate {
     
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let offset = scrollView.contentOffset.y
-        let fi
-        
-        if offset <
-    }
-    
-//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let offset = scrollView.contentOffset.y
+//        let finishY = locationStackView.frame.origin.y + locationStackView.frame.size.height
 //
-//        let contentOffset = locationStackView.frame.size.height + locationStackViewConstraintTop.constant
+//        var newCollectionFrame = CGRect()
 //
+//        if offset < finishY {
+//            print(UIScreen.main.bounds.size.height)
+//            print(finishY)
+//            print(offset)
+//            let newMaskHeight = UIScreen.main.bounds.size.height - finishY + offset
+//            print("--> \(newMaskHeight)")
+//            let newMaskStartingY = collectionView.frame.origin.y
+//            newCollectionFrame = CGRect(x: 0, y: newMaskStartingY, width: UIScreen().bounds.size.width, height: newMaskHeight)
 //
-//        scrollView.setContentOffset(CGPoint(x: 0, y: contentOffset), animated: true)
+//        }
+//        
+//        collectionView.frame = newCollectionFrame
 //    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+
+        let contentOffset = locationStackView.frame.size.height + locationStackViewConstraintTop.constant
+
+
+        scrollView.setContentOffset(CGPoint(x: 0, y: contentOffset), animated: true)
+    }
 }
 
+// MARK: - CLLocationManagerDelegate
+
+extension MainViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        
+        userLocationCoordinate?.longitude = locValue.longitude
+        userLocationCoordinate?.latitude = locValue.latitude
+    }
+}
